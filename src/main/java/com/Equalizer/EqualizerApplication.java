c69146cee2d48e9fe021e06e43dc32bb24d2b0a1
MyllerT <millertavares@yahoo.com.br>
2023-11-17 08:36:23 -0300

    Submissão inicial

---
+++ src/main/java/com/Equalizer/EqualizerApplication.java
@@ -0,0 +1,11 @@
+package com.Equalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqualizerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EqualizerApplication.class, args);
+	}
+}
